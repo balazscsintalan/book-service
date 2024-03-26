8caee7d7d53afd92090c4d8637e77523eff59cc2
balazs <csintalan.balazs@danubiusinfo.hu>
2024-03-26 12:29:21 +0100

    author repository

---
+++ src/main/java/hu/danubius/bookservice/entity/AuthorEntity.java
@@ -0,0 +1,37 @@
+package hu.danubius.bookservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "AUTHOR")
+public class AuthorEntity {
+
+    @Id
+    @Column(name = "ID", nullable = false, updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/hu/danubius/bookservice/service/AuthorService.java
+++ src/main/java/hu/danubius/bookservice/service/AuthorService.java
@@ -1,6 +1,8 @@
 package hu.danubius.bookservice.service;
 
+import hu.danubius.bookservice.entity.AuthorEntity;
 import hu.danubius.bookservice.model.Author;
+import hu.danubius.bookservice.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,11 +10,24 @@ import java.util.List;
 @Service
 public class AuthorService {
 
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
     public List<Author> getAuthors() {
-        return List.of(new Author("Brandon Sanderson"));
+        return authorRepository.findAll()
+            .stream()
+            .map(authorEntity -> new Author(authorEntity.getName()))
+            .toList();
     }
 
     public Author getAuthorById(Long id) {
-        return new Author("Brandon Sanderson");
+        AuthorEntity author = authorRepository
+            .findById(id)
+            .orElseThrow(() -> new IllegalStateException("Author not found by id"));
+
+        return new Author(author.getName());
     }
 }
